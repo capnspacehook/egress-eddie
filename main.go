fee6f11a3d2783cbee6569dd7b9d158b4a2099a4
Andrew LeFevre <jalefevre@liberty.edu>
2021-11-12 11:42:21 -0500

    make logging more configurable

--- main.go
+++ main.go
@@ -10,16 +10,29 @@ import (
 	"go.uber.org/zap"
 )
 
-var configPath string
+var (
+	configPath string
+	debug      bool
+	logPath    string
+)
 
 func init() {
 	flag.StringVar(&configPath, "c", "egress-eddie.toml", "path of the config file")
+	flag.BoolVar(&debug, "d", false, "enable debug logging")
+	flag.StringVar(&logPath, "l", "egress-eddie.log", "path to log to")
 }
 
 func main() {
 	flag.Parse()
 
-	logger, err := zap.NewDevelopment()
+	logCfg := zap.NewProductionConfig()
+	logCfg.OutputPaths = []string{logPath}
+	if debug {
+		logCfg.Level.SetLevel(zap.DebugLevel)
+	}
+	logCfg.DisableCaller = true
+
+	logger, err := logCfg.Build()
 	if err != nil {
 		log.Fatalf("error creating logger: %v", err)
 	}
